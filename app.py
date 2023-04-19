395daab8760c802714912923d7463546b0d800ad
petgeb <131294626+petgeb@users.noreply.github.com>
2023-04-19 20:03:06 +0200

    Add files via upload
    h

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "supertest_8987"
+
+@app.route("/hello")
+def index():
+    flash("What's your name?")
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", great to see you.")
+    return render_template("index.html")
+    
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5001)
